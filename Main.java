7078ff9b3f1a478c075693a9d842d518787e83bb
Taylor Romoser <TRomoser@gmail.com>
2025-03-22 23:28:13 -0700

    changed wording in report

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 /*
  * @author Taylor Romoser
  * @date 3/22/25
- *  To determine the most likely date of the camping trip, I wrote a program that:
+ *  To determine the most likely date of the camping trip, I wrote 
+ *  a program that:
  * 1. Read all the radiation samples from the log file.
  * 2. Found the maximum counts per minute recorded.
  * 3. Collected all samples within 5 counts of that maximum.
@@ -11,15 +12,15 @@
  * I figured that a camping trip at higher elevation (4900 ft) 
  * would not result in a single high reading, but a full day of elevated 
  * radiation levels. By grouping and counting high readings per day,
- * I was able to identify the most likely date I was at a higher elevation.
+ * I was able to identify the most likely date you were at a higher elevation.
  * 
  * Based on this method, the most likely camping trip date was: June 1, 2019
- * But based on data you likely started the trip on May 31, 2019 and ended on 
- * June 2, 2019
+ * But based on the surrounding data you likely started the trip on 
+ * May 31, 2019 and ended on June 2, 2019
  * 
  * Challenges/roadblocks:
- * - I initially was challenged by the first lines of the file, I needed to
- *  skip the ones that were shorter than 3 entries
+ * - I initially was challenged by the first lines of the source file, I needed
+ *  to skip the ones that were shorter than 3 entries
  * - At one point I tried chaining methods like
  *  sample.getCountsPerMinute().getDateTime(), then realized I needed to
  *  isolate them in variables then do the parsing
@@ -30,8 +31,8 @@
  * - I originally tried using Duration to find a long streak of high readings,
  *  but realized I just needed to find the day with the most elevated samples.
  * - A malformed date ("2019-05-32 00:11") caused parsing errors. I handled it 
- * with a try/catch block to skip the bad line. If corrected, it would add one 
- * more high reading to June 1, 2019 (bringing the total to 22).
+ *  with a try/catch block to skip the bad line. If corrected, it would add one 
+ *  more high reading to June 1, 2019 (bringing the total to 22).
  */
 
 import java.io.*;
@@ -46,7 +47,7 @@ public class Main {
     ArrayList<RadiationSample> samples = new ArrayList<>();
     int maxCountPerMin = 0;
     
-    // Read text file and add to sample array list, 
+    // Read text file and add to sample list, 
     // while also finding the maxCount
     try(BufferedReader reader = new BufferedReader(new FileReader(FILENAME))) {
       String line;
@@ -69,7 +70,7 @@ public class Main {
       System.err.println("Error reading file: " + e.getMessage());
     }
     
-    // Add samples with count within 5 of maxCount to new array list
+    // Add samples with count within 5 of maxCount to new list
     ArrayList<RadiationSample> maxCountSamples = new ArrayList<>();
     for(RadiationSample sample: samples) {
       if(sample.getCountsPerMinute() >= maxCountPerMin - 5) {
@@ -78,14 +79,15 @@ public class Main {
     }
 
     // Print table of results
-    printTable("Radiation Count by Date", maxCountSamples);
+    printSamplesInTable("Radiation samples with CPM >= (max - 5)", maxCountSamples);
 
     // Create hash map with dates as keys and samples as values
     Map<LocalDate, List<RadiationSample>> groupedByDate = new HashMap<>();
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy H:mm");
     for(RadiationSample sample: maxCountSamples) {
       // Parse date from dateTime string
-      // try catch because there is a line with date 2019-05-32 00:11
+      // try/catch to skip a line in the source 
+      // with date 2019-05-32 00:11 (invalid)
       try {
         LocalDateTime dateTime = LocalDateTime.parse(sample.getDateTime(), formatter);
         LocalDate date = dateTime.toLocalDate();
@@ -108,10 +110,10 @@ public class Main {
 
     // Print camping date and table of samples that day
     System.out.println("Most Likely Camping trip date: " + campDate + ", number of high counts that day : " + mostReadings);
-    printTable("Samples that day", groupedByDate.get(campDate));
+    printSamplesInTable("Samples that day", groupedByDate.get(campDate));
   }
 
-  public static void printTable(String title, List<RadiationSample> samples) {
+  public static void printSamplesInTable(String title, List<RadiationSample> samples) {
     int totalWidth = 30;
     int padding = (totalWidth - title.length()) / 2;
 
